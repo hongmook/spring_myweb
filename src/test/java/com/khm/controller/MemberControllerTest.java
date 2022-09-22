2db69bb7251b16607a0ce4a67b9c75dc8ab86ed2
hongmook <ghdanr6589@naver.com>
2022-09-22 15:17:13 +0900

    JDBCTest - controller test

---
+++ src/test/java/com/khm/controller/MemberControllerTest.java
@@ -0,0 +1,57 @@
+package com.khm.controller;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+								  "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+public class MemberControllerTest {
+
+	private static final Logger log = LoggerFactory.getLogger("MemberControllerTest.class");
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc; //요청, 응답
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+		log.info("mockMvc setup...");
+	}
+	
+	@Test
+	public void test() {
+		try {
+			String rs = mockMvc.perform(MockMvcRequestBuilders.post("/login")
+										.param("id", "joy2").param("pw", "123123")
+										).andReturn().getModelAndView().getViewName();
+			
+			log.info(rs);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+}
