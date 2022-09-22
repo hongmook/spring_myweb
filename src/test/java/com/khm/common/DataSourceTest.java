77d93f0b595a86225c97752dc5ee049cda1854b7
hongmook <ghdanr6589@naver.com>
2022-09-22 14:51:34 +0900

    JDBCTest - DataSourceTest

--- src/test/java/com/khm/common/DataSourceTest.java
+++ src/test/java/com/khm/common/DataSourceTest.java
@@ -4,34 +4,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
-public class JDBCTest {
+public class DataSourceTest {
 
-	private static final Logger log = LoggerFactory.getLogger("JDBCTest.class");
+	private static final Logger log = LoggerFactory.getLogger("DataSourceTest.class");
+	
+	@Autowired
+	private DataSource ds;
 	
 	@Test
 	public void test() {
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
-			Connection conn = DriverManager.getConnection(
-					"jdbc:oracle:thin:@localhost:1521:xe",
-					"joy",
-					"0070");
+			Connection conn = ds.getConnection();
 			log.info(conn.toString());
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
